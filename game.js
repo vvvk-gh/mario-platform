575261393536c4c34682b25f68936167eb009f50
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-26 01:36:58 +0530

    sprite sheets

--- game.js
+++ game.js
@@ -15,6 +15,9 @@ let config = {
         preload : preload,
         create : create,
         update : update
+    },
+    physics : {
+
     }
 
 }
@@ -28,7 +31,8 @@ function preload() {
     this.load.image("ground" , "assets/topground.png")
     this.load.image("sky" , "assets/background.png")
     //add sprite sheet : make sure the width and height of the image is multiples of 2
-    this.load.spritesheet("dude", "assests/dude.png") //open game art library for more sprite sheets
+    //frameWidth and frameHeight is the width and height of each single image in the sprite
+    this.load.spritesheet("dude","assets/dude.png", {frameWidth:32, frameHeight:48}) //open game art library for more sprite sheets
 }
 
 
@@ -49,7 +53,7 @@ function create() {
     background.depth = -1;
 
     //adding player : (x,y,image,defalut frame)
-    this.add.sprite(100,100,"dude")
+    this.add.sprite(100,100,"dude",4)
 }
 //game loop
 function update() {
