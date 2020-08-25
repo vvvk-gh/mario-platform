1fcaa219c8fa819ca14aa49a30cce91366bc58e1
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-26 01:45:48 +0530

    added physics to the player and ground

--- game.js
+++ game.js
@@ -9,15 +9,21 @@ let config = {
         height : 600,
     },
 
-    backgroundColor: 0xff0000,
+    backgroundColor: 0xffffc0,
 
     scene : {
         preload : preload,
         create : create,
         update : update
     },
+    //adding physics to canvas/scene
     physics : {
-
+        default :'arcade',
+        arcade:{
+            gravity :{
+                y:1000
+            }
+        }
     }
 
 }
@@ -31,7 +37,7 @@ function preload() {
     this.load.image("ground" , "assets/topground.png")
     this.load.image("sky" , "assets/background.png")
     //add sprite sheet : make sure the width and height of the image is multiples of 2
-    //frameWidth and frameHeight is the width and height of each single image in the sprite
+    //frameWidth and frameHeight is the width and height of each single image in the spritesheet
     this.load.spritesheet("dude","assets/dude.png", {frameWidth:32, frameHeight:48}) //open game art library for more sprite sheets
 }
 
@@ -49,11 +55,16 @@ function create() {
 
     let background = this.add.sprite(0,0,"sky")
     background.setOrigin(0,0)
-    background.displayWidth = W; 
+    background.displayWidth = W;
+    //background.displayHeight = H; 
     background.depth = -1;
 
     //adding player : (x,y,image,defalut frame)
-    this.add.sprite(100,100,"dude",4)
+    //adding physics to the player
+    let player = this.physics.add.sprite(100,100,"dude",4)
+
+    //adding physics to the existing one can be done using below
+    this.physics.add.existing(ground);
 }
 //game loop
 function update() {
