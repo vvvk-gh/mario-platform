e63409a415556d1894a16ee9cab61e2c9fcdc417
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-25 23:14:08 +0530

    adding sprites in canvas

--- game.js
+++ game.js
@@ -1,22 +1,44 @@
 //basic setup
 
 let config = {
-    type :Phaser.CANVAS,
-    width : 700,
-    height : 300,
+    type :Phaser.AUTO, //it chooses the best option automatically between canvas and webgl
+    
+    scale : {
+        mode :Phaser.Scale.FIT, // refer scaler class in documentation
+        width : 800,
+        height : 600,
+    },
+
+    backgroundColor: 0xff0000,
+
+    scene : {
+        preload : preload,
+        create : create,
+        update : update
+    }
+
 }
 
 
 let game = new Phaser.Game(config)
 
 
-function load() {
-    console.log(`load`)
+function preload() {
+    //load images 
+    this.load.image("ground" , "assets/topground.png")
 }
 
 
 function create() {
-    console.log(`Create`)
+    
+    W = game.config.width;
+    H = game.config.height;
+
+    let ground = this.add.sprite(0,H-128, "ground")
+    //intailly phasersprites takes the point from the center of the image by default
+    //now lets set them to starting points
+    ground.setOrigin(0,0);
+    
 }
 
 //game loop
